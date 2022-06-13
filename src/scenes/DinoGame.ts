4a3a5f4252083793fabcf4769327a449851eba79
Anh Vũ <thomas.maihoanganhvu@gmail.com>
2022-06-13 14:44:39 +0700

    Add expanding ground

--- src/scenes/DinoGame.ts
+++ src/scenes/DinoGame.ts
@@ -20,12 +20,14 @@ export default class DinoGame extends Phaser.Scene {
 
     private ground!: Phaser.GameObjects.TileSprite;
     private dino!: Phaser.GameObjects.Sprite;
+    private startTrigger!: Phaser.GameObjects.Sprite;
 
     private scoreLabel!: Phaser.GameObjects.Text;
     private highScoreLabel!: Phaser.GameObjects.Text;
 
     private score = 0;
     private gameSpeed = 10;
+    private isGameRunning = false;
 
     private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
 
@@ -38,6 +40,7 @@ export default class DinoGame extends Phaser.Scene {
     init() {
         this.score = 0;
         this.gameSpeed = 10;
+        this.isGameRunning = false;
     }
 
     create() {
@@ -48,13 +51,13 @@ export default class DinoGame extends Phaser.Scene {
         this.hitSound = this.sound.add(DinoAudioKeys.Hit, { volume: 0.2 });
         this.reachSound = this.sound.add(DinoAudioKeys.Reach, { volume: 0.2 });
 
-        const startTrigger= this.physics.add.sprite(1, 10, "").setOrigin(0, 0).setImmovable().setVisible(false);
+        this.startTrigger = this.physics.add.sprite(1, 10, "").setOrigin(0, 0).setImmovable().setVisible(false);
 
         // ground
-        this.ground = this.add.tileSprite(0, height, width, 26, DinoTextureKeys.Ground).setOrigin(0, 1);
+        this.ground = this.add.tileSprite(0, height, 88, 26, DinoTextureKeys.Ground).setOrigin(0, 1);
         this.dino = this.physics.add
             .sprite(1, height, DinoTextureKeys.Dino)
-            .play(DinoAnimationKeys.DinoRun)
+            .play(DinoAnimationKeys.DinoIdle)
             .setCollideWorldBounds(true)
             .setGravityY(5000)
             .setBodySize(88, 92)
@@ -81,6 +84,7 @@ export default class DinoGame extends Phaser.Scene {
 
         this.physics.world.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height - 1);
         this.cursors = this.input.keyboard.createCursorKeys();
+        this.physics.add.overlap(this.dino, this.startTrigger, this.handleStartTrigger, undefined, this);
     }
 
     handleInputs() {
@@ -122,7 +126,45 @@ export default class DinoGame extends Phaser.Scene {
         }
     }
 
+    /**
+     * Turn off start trigger when start the game
+     * @returns None
+     */
+    handleStartTrigger() {
+        const { width, height } = this.scale;
+        const startTriggerBody = this.startTrigger.body as Phaser.Physics.Arcade.Body;
+        if (this.startTrigger.y == 10) {
+            this.startTrigger.setOrigin(0, 1);
+            startTriggerBody.reset(0, height); // bring trigger start to foot of dino for else branch
+        } else {
+            this.startTrigger.setActive(false);
+            const body = this.dino.body as Phaser.Physics.Arcade.Body;
+            const startEvent = this.time.addEvent({
+                delay: 1000 / 60,
+                loop: true,
+                callbackScope: this,
+                callback: () => {
+                    body.setVelocityX(80);
+                    this.dino.play(DinoAnimationKeys.DinoRun, true);
+
+                    if (this.ground.width < width) {
+                        console.log("grow");
+                        this.ground.width += 17 * 2;
+                    }
+
+                    if (this.ground.width >= 1000) {
+                        this.ground.width = width;
+                        this.isGameRunning = true;
+                        body.setVelocityX(0);
+                        startEvent.remove();
+                    }
+                },
+            });
+        }
+    }
+
     update() {
+        // if (!this.isGameRunning) return;
         this.ground.tilePositionX += this.gameSpeed;
         this.handleInputs();
     }
